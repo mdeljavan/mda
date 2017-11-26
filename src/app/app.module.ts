14da46b7812540548dff253ea1ed596121d6aab5
Mohammadreza Deljavan <31951305+mdeljavan@users.noreply.github.com>
2017-11-26 18:11:49 +0330

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { FollowersComponent } from './followers/followers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    FollowersComponent
   ],
   imports: [
     BrowserModule
